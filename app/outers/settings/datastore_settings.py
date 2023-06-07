19ee70d769abb61f84bd06838273c4bcf378e5a6
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-06-08 02:49:56 +0700

    Update setting.

--- app/outers/settings/datastore_settings.py
+++ app/outers/settings/datastore_settings.py
@@ -1,18 +1,17 @@
-import os
 from typing import Optional
 
 from pydantic import BaseSettings
 
 
 class DatastoreSetting(BaseSettings):
-    DS_DIALECT: str = os.getenv("DS_DIALECT")
-    DS_HOST: str = os.getenv("DS_HOST")
-    DS_PORT: str = os.getenv("DS_PORT")
-    DS_USER: str = os.getenv("DS_USER")
-    DS_PASSWORD: str = os.getenv("DS_PASSWORD")
-    DS_DATABASE: str = os.getenv("DS_DATABASE")
+    DS_DIALECT: str
+    DS_HOST: str
+    DS_PORT: str
+    DS_USER: str
+    DS_PASSWORD: str
+    DS_DATABASE: str
     URL: Optional[str]
 
-    def __init__(self, **kwargs: any):
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.URL = f"{self.DS_DIALECT}://{self.DS_USER}:{self.DS_PASSWORD}@{self.DS_HOST}:{self.DS_PORT}/{self.DS_DATABASE}"
