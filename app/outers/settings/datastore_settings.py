6f9515f2628706b774e1c61d2739f76fc59fd944
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-06-08 01:37:53 +0700

    Update setting.

--- app/outers/settings/datastore_settings.py
+++ app/outers/settings/datastore_settings.py
@@ -16,12 +16,6 @@ class DatastoreSetting(BaseSettings):
 
     def __init__(self, **kwargs: any):
         super().__init__(**kwargs)
-        self.DS_DIALECT = os.environ.get("DS_DIALECT")
-        self.DS_HOST = os.environ.get("DS_HOST")
-        self.DS_PORT = os.environ.get("DS_PORT")
-        self.DS_USER = os.environ.get("DS_USER")
-        self.DS_PASSWORD = os.environ.get("DS_PASSWORD")
-        self.DS_DATABASE = os.environ.get("DS_DATABASE")
         self.URL = f"{self.DS_DIALECT}://{self.DS_USER}:{self.DS_PASSWORD}@{self.DS_HOST}:{self.DS_PORT}/{self.DS_DATABASE}"
 
 
