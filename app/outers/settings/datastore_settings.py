ac9070dacae020a0be279eff23ff6142a9f225b4
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-06-08 01:37:40 +0700

    Update setting.

--- app/outers/settings/datastore_settings.py
+++ app/outers/settings/datastore_settings.py
@@ -5,17 +5,23 @@ from pydantic import BaseSettings
 
 
 class DatastoreSetting(BaseSettings):
-    DS_DIALECT: str = os.environ.get("DS_DIALECT")
-    DS_HOST: str = os.environ.get("DS_HOST")
-    DS_PORT: str = os.environ.get("DS_PORT")
-    DS_USER: str = os.environ.get("DS_USER")
-    DS_PASSWORD: str = os.environ.get("DS_PASSWORD")
-    DS_DATABASE: str = os.environ.get("DS_DATABASE")
+    DS_DIALECT: str
+    DS_HOST: str
+    DS_PORT: str
+    DS_USER: str
+    DS_PASSWORD: str
+    DS_DATABASE: str
     URL: Optional[str]
 
-    class Config:
-        env_file = ".env"
 
     def __init__(self, **kwargs: any):
         super().__init__(**kwargs)
+        self.DS_DIALECT = os.environ.get("DS_DIALECT")
+        self.DS_HOST = os.environ.get("DS_HOST")
+        self.DS_PORT = os.environ.get("DS_PORT")
+        self.DS_USER = os.environ.get("DS_USER")
+        self.DS_PASSWORD = os.environ.get("DS_PASSWORD")
+        self.DS_DATABASE = os.environ.get("DS_DATABASE")
         self.URL = f"{self.DS_DIALECT}://{self.DS_USER}:{self.DS_PASSWORD}@{self.DS_HOST}:{self.DS_PORT}/{self.DS_DATABASE}"
+
+
